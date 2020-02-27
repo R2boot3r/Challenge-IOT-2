62c88cba11d212955ae81316898495694529866a
julien <julien.choveton@gmail.com>
2020-02-28 00:19:25 +0100

    modifs code

--- Code Python/Serialwriter.py
+++ Code Python/Serialwriter.py
@@ -5,18 +5,18 @@ Spyder Editor
 This is a temporary script file.
 """
 import serial
-import time
+import datetime
 import csv
 
 
 
 #il va falloir mettre un truc qui prend directement le bon periphérique
-ser = serial.Serial('/dev/ttyACM2')
+ser = serial.Serial('COM4') #/dev/ttyACM2
 ser.flushInput()
 
 #contient l'ensemble des ids de nos nodes lora
 
-List_node_lora = [123,234] 
+List_node_lora = ['123','321'] 
 
 #attention ne pas oublier de mettre a la fin du fichier par la suite !
 
@@ -24,10 +24,16 @@ while True:
 
         ser_bytes = ser.readline()
         decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+        print(decoded_bytes)
         
-
         
+        data = decoded_bytes.split(";")
         
-        with open("test_data.csv","a") as f:
-            writer = csv.writer(f,delimiter=";")
-            writer.writerow([decoded_bytes,time.time()])
+        if data[0] in List_node_lora:
+            #print("bonjour")
+            with open("test_data.csv","a") as f:
+                writer = csv.writer(f,delimiter=";")
+                now = datetime.datetime.now()
+                string = now.strftime("%Y-%m-%d %H:%M:%S")+";"+decoded_bytes
+                print(string)
+                writer.writerow([string])
